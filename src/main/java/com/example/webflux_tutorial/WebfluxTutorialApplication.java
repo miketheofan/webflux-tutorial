44b7969135691770fb981f2f7850a5ac6dea4bda
miketheofan <miketheofan@gmail.com>
2025-07-09 18:47:24 +0300

    Initial commit

---
+++ src/main/java/com/example/webflux_tutorial/WebfluxTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTutorialApplication.class, args);
+	}
+
+}
